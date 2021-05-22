0000efa6899edd4a8b1e9a6401318eb35259791d
Agnieszka Brown <abrown2012@icloud.com>
2021-05-22 13:01:22 -0400

    commit tree 3dc91ebfda520f0172b828a798b2495316d2bf5f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+
+
+const map = (sourceArray, fun) => {
+    return sourceArray.map(x => fun(x))
+}
+
+const reduce = (sourceArray, fun, st=0) => {
+    if (st ===0) {
+    return (sourceArray.reduce((x, y) => fun(x, y)))
+    } else {
+        return st+(sourceArray.reduce((x, y) => fun(x, y)))
+    }
+  }
